2da75b1c6480f0d44ea1ab92edf313d905896d44
lucasvianav <oviana@outlook.com>
2020-12-18 12:46:41 -0300

    main-2.c --> 10/10

--- Atv-13/main-2.c
+++ Atv-13/main-2.c
@@ -47,29 +47,52 @@ void quickSort(point *vector, int leftIndex, int rightIndex){
     return;
 }
 
-void quickSortY(point *vector, int leftIndex, int rightIndex){
-    if(leftIndex < rightIndex){
-        float pivot = vector[(int)((rightIndex+leftIndex)/2)].y;
-        int left = leftIndex, right = rightIndex;
+// Receives a vector and the position of two sorted subvectors inside it - merges both of them into a fully sorted one
+void mergeSubvectors(point *vector, int firstLeftIndex, int firstRightIndex, int secondLeftIndex, int secondRightIndex){
+    int vectorSize = (firstRightIndex - firstLeftIndex + 1) + (secondRightIndex - secondLeftIndex + 1);
 
-        while(1){
-            // Selects elements from the left that are >= to the pivot
-            while(vector[left].y < pivot){ left++; }
+    point *tmp = (point *)malloc(vectorSize * sizeof(point)); // Temporary vector in which the interpolation will occur
+    
+    int i, j, k = 0; // Auxiliar variables that'll index the for-loops
 
-            // Selects elements from the right that are <= to the pivot
-            while(vector[right].y > pivot){ right--; }
+    // Loops through both subvectors, merging them
+    for(i = firstLeftIndex, j = secondLeftIndex; k < vectorSize; k++){
 
-            // If the swapping the above selected elements is worth it, do it
-            if(left < right){ swapElements(vector, left++, right--); } // And pass to the next element
-
-            // If the swap is not worth it, the vector was successfully partitionted
-            else{ break; }
-
+        // If there are still elements in both subvector
+        if(i <= firstRightIndex && j <= secondRightIndex){ 
+            // Compares them and copies the lowest one to tmp
+            if(vector[i].y < vector[j].y){ tmp[k] = vector[i++]; }
+            else{ tmp[k] = vector[j++]; }
         }
 
-        // Sorts the partitions
-        quickSort(vector, leftIndex, right);
-        quickSort(vector, right+1, rightIndex);
+        // If not, copies the remaining element to tmp
+        else if(i <= firstRightIndex){ tmp[k] = vector[i++]; }
+        else if(j <= secondRightIndex){ tmp[k] = vector[j++]; }
+
+    }
+
+    // Copies the sorted vector in tmp to the original one, replacing the subvectors
+    for(i = firstLeftIndex, j = secondLeftIndex, k = 0; k < vectorSize; k++){
+        if(i <= firstRightIndex){ vector[i++] = tmp[k]; }
+        else{ vector[j++] = tmp[k]; }
+
+    }
+
+    free(tmp); tmp = NULL;
+
+    return;
+}
+
+// MERGE SORT
+void mergeSort(point *vector, int leftIndex, int rightIndex){
+    int vectorSize = rightIndex - leftIndex + 1;
+
+    if (vectorSize > 1){
+        int middleIndex = (rightIndex + leftIndex) / 2;
+
+        mergeSort(vector, leftIndex, middleIndex);
+        mergeSort(vector, middleIndex + 1, rightIndex);
+        mergeSubvectors(vector, leftIndex, middleIndex, middleIndex + 1, rightIndex);
     }
 
     return;
@@ -98,7 +121,8 @@ float auxGetLowestDist(point *points, int leftIndex, int rightIndex){
         }
     }
 
-    quickSortY(strip, 0, stripSize-1);
+    // quickSortY(strip, 0, stripSize-1);
+    mergeSort(strip, 0, stripSize-1);
 
     float div = (strip[0].x > points[leftIndex].x && strip[stripSize-1].x < points[rightIndex].x) 
         ? auxGetLowestDist(strip, 0, stripSize-1) 
